f4d47ec77918a8982b1f5ff61aacd4d2232256d7
Julian Nicholls <juliannicholls29@gmail.com>
2014-04-12 14:24:56 +0100

    Updated grid with weightings and word collection.
    The consonant and vowel weightings have been changed, both within
    and between each.
    Rudimentary word collection is in place.
    After grid generation, islands of three letters the same have
    the middle one re-generated.

--- grid.rb
+++ grid.rb
@@ -5,14 +5,20 @@ module WordThing
   class Grid
     include Constants
 
-    CONS    = 'BBCCDDDFFGGGHHHJKKLLMMNNNPPQRRRSSSTTTTVVWXYYZ'
-    VOWELS  = 'AAAAEEEEEIIIOOOOUU'  # E5 A4 O$ I3 U2
+    attr_accessor :word
+
+    # These weightings are based on an analysis of wtwords.txt
+
+    CONS    = 'BBBCCCCCCCDDDDDFFGGGGHHHHJKKLLLLLLLLMMMMMNNNNNNNNNNN' \
+              'PPPPPQRRRRRRRRRRRSSSSSSSSSSSSSSSTTTTTTTTTTTTTTTVVWXYYYZ'
+    VOWELS  = 'AAAAAAAAAAAEEEEEEEEEEEEEEEIIIIIIIIIIIIOOOOOOOOOUUUU'
 
     def initialize( surface, columns = COLUMNS, rows = ROWS )
       @window         = surface
       @columns, @rows = columns, rows
 
-      @grid = build_grid
+      @grid           = build_grid
+      @word           = ''
     end
 
     def draw
@@ -24,54 +30,68 @@ module WordThing
     def toggle_select( position )
       column  = (position.x - GRID_ORIGIN.x) / BLOCK_SIZE
       row     = (position.y - GRID_ORIGIN.y) / BLOCK_SIZE
-      
-      @grid[column][row][:selected] = !@grid[column][row][:selected]
+
+      process_selection( column, row )
     end
-    
+
     private
 
+    def process_selection( col, row )
+      cell = @grid[col][row]
+      if cell[:selected]
+        cell[:selected] = false
+        @word.pop if @word.size > 0 && @word[-1] == cell[:letter]
+      else
+        cell[:selected] = true
+        @word << cell[:letter]
+      end
+    end
+    
     def build_grid
       @grid = Array.new( @columns ) do
-        Array.new( @rows ) do
-          {
-            letter:   random_letter,
-            selected: false
-          }
-        end
+        Array.new( @rows ) { { letter: random_letter, selected: false } }
       end
-      
+
       post_process
     end
 
-    # A biased set of random letters
-    # 60% consonant, 40% vowel
-    # E > A > O > I > U
-    # T > NRS > HGY >>>> ZXWQ
+    # 62% consonant, 38% vowel with the weightings above
 
     def random_letter
-      rand( 10 ) < 6 ? CONS[rand CONS.size] : VOWELS[rand VOWELS.size]
+      rand( 100 ) < 62 ? CONS[rand CONS.size] : VOWELS[rand VOWELS.size]
     end
-    
+
     def post_process
       add_us_to_qs
+      change_triples
     end
-    
+
     def add_us_to_qs
       @grid.each_with_index do |column, col|
         column.each_with_index do |cell, row|
           if cell[:letter] == 'Q'
-            cell[:selected] = true
             n = neighbours( col, row )
             unless n.any? { |c, r| @grid[c][r][:letter] == 'U' }
               r = n[rand n.size]
               @grid[r[0]][r[1]][:letter] = 'U'
-              @grid[r[0]][r[1]][:selected] = true
             end
           end
         end
       end
     end
-    
+
+    def change_triples
+      @grid.each_with_index do |column, col|
+        column.each_with_index do |cell, row|
+          n = neighbours( col, row )
+
+          if n.select { |c, r| @grid[c][r][:letter] == cell[:letter] }.size > 1
+            cell[:letter] = random_letter
+          end
+        end
+      end
+    end
+
     def neighbours( x, y )
       neighs = []
       (-1..1).each do |xd|
@@ -80,23 +100,26 @@ module WordThing
           neighs << [x + xd, y + yd] if in_grid( x + xd, y + yd )
         end
       end
-      
+
       neighs
     end
-    
+
     def in_grid( x, y )
       x.between?( 0, COLUMNS - 1) && y.between?( 0, ROWS - 1)
     end
 
     def render( col, row, cell )
       font      = @window.fonts[:letter]
-      size      = font.measure( cell[:letter] )
-      image     = cell[:selected] ? @window.images[:selected] : @window.images[:letter]
+      ltr_ctr   = font.centred_in( cell[:letter], Size.new( BLOCK_SIZE, BLOCK_SIZE ) )
       point     = GRID_ORIGIN.offset( col * BLOCK_SIZE, row * BLOCK_SIZE )
-      ltr_point = point.offset( (BLOCK_SIZE - size.width) / 2, (BLOCK_SIZE - size.height) / 2 )
+      ltr_point = point.offset( ltr_ctr )
 
-      image.draw( point.x, point.y, 1 )
+      background_image( cell ).draw( point.x, point.y, 1 )
       font.draw( cell[:letter], ltr_point.x, ltr_point.y, 2, 1, 1, BLUE )
-    end    
+    end
+
+    def background_image( cell )
+      cell[:selected] ? @window.images[:selected] : @window.images[:letter]
+    end
   end
 end
