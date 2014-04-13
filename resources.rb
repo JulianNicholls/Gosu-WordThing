7f74f545be227dd77792edfb1afc74e3240d5770
Julian Nicholls <juliannicholls29@gmail.com>
2014-04-13 21:00:29 +0100

    Added sounds and fonts.

--- resources.rb
+++ resources.rb
@@ -7,7 +7,8 @@ module WordThing
       {
         letter: Gosu::Font.new( window, 'Arial', 30 ),
         word:   Gosu::Font.new( window, 'Arial', 20 ),
-        small:  Gosu::Font.new( window, 'Arial', 11 )
+        small:  Gosu::Font.new( window, 'Arial', 11 ),
+        time:   Gosu::Font.new( window, 'Arial', 25 )
       }
     end
 
@@ -21,12 +22,8 @@ module WordThing
 
     def self.sounds( window )
       {
-        flip: Gosu::Sample.new( window, 'media/Blip.wav' ),
-        tada: [Gosu::Sample.new( window, 'media/tada.wav' ),
-               Gosu::Sample.new( window, 'media/alleluia.wav' ),
-               Gosu::Sample.new( window, 'media/shazam2.wav' ),
-               Gosu::Sample.new( window, 'media/ww_kewl.wav' ),
-               Gosu::Sample.new( window, 'media/yeehaw.wav' )]
+        ok:   Gosu::Sample.new( window, 'media/ok.wav' ),
+        uhuh: Gosu::Sample.new( window, 'media/uhuh.wav' ),
       }
     end
   end
