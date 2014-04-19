1964c7ef46382243c71d9f19824a52ae2c2f058b
Julian Nicholls <juliannicholls29@gmail.com>
2014-04-19 21:56:40 +0100

    Updated with Rubocop advice.

--- button.rb
+++ button.rb
@@ -1,26 +1,29 @@
 module WordThing
+  # UI Button that calls a passed block when press is called with a position
+  # inside the button region.
   class Button
     include GosuEnhanced
-    
-    def initialize( surface, position, size, text, text_colour, bkgr_colour, &press_block )
+
+    def initialize( surface, pos, size, text, text_clr, bkgr_clr, &press_block )
       @window   = surface
-      @region   = Region.new( position, size )
+      @region   = Region.new( pos, size )
       @caption  = text
-      @text     = text_colour
-      @bkgr     = bkgr_colour
+      @text     = text_clr
+      @bkgr     = bkgr_clr
       @block    = press_block
     end
-    
+
     def draw
       @region.draw( @window, 2, @bkgr )
+
       font = @window.fonts[:button]
       pos  = @region.position.offset( font.centred_in( @caption, @region.size ) )
       font.draw( @caption, pos.x, pos.y, 2, 1, 1, @text )
     end
-    
+
     def press( pos )
       return false unless @region.contains? pos
-      
+
       instance_eval( &@block )
     end
   end
