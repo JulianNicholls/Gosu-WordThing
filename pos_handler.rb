5a1a3b1795c7afd1f1281d891523f0e9f838faf6
Julian Nicholls <juliannicholls29@gmail.com>
2014-04-19 18:20:03 +0100

    PositionHandler added.
    I'm not happy with it at the moment.

---
+++ pos_handler.rb
@@ -0,0 +1,25 @@
+require 'pp'
+
+module WordThing
+  class PositionHandlers
+    def initialize( &block )
+      @list = []
+      instance_eval( &block ) if block_given?
+    end
+    
+    def add( object, method )
+      @list << [object, method]
+    end
+    
+    # Pass the position to each listed object and return when someone
+    # has handled it.
+    
+    def pass( pos )
+      return if pos.nil?
+
+      @list.each do |object, method|
+        return if object.__send__( method, pos )
+      end
+    end
+  end
+end
