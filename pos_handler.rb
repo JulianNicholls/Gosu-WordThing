f784f993b85b5f2a47cfe64c70fd8c35ac3fc10c
Julian Nicholls <juliannicholls29@gmail.com>
2014-04-19 21:57:17 +0100

    Added comments

--- pos_handler.rb
+++ pos_handler.rb
@@ -1,19 +1,21 @@
 require 'pp'
 
 module WordThing
+  # Holds a list of mouse position handlers.
+  # I'm not sure I'm entirely happy with this yet
   class PositionHandlers
     def initialize( &block )
       @list = []
       instance_eval( &block ) if block_given?
     end
-    
+
     def add( object, method )
       @list << [object, method]
     end
-    
+
     # Pass the position to each listed object and return when someone
     # has handled it.
-    
+
     def pass( pos )
       return if pos.nil?
 
